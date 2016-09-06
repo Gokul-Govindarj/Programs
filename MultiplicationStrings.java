c45ae363bb388c20d3e1744b13d4c518e5678331
Gokul-Govindarj <gocoolg6696@gmail.com>
2016-09-06 09:03:04 +0530

    MultiplicationStrings.java

---
+++ MultiplicationStrings.java
@@ -0,0 +1,12 @@
+public class MultiplicationStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.next();
+		String str2 = sc.next();
+		sc.close();
+		System.out.println(Long.valueOf(str1)*Long.valueOf(str2));
+	}
+
+}
